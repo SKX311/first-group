1345523d634e5ec54ab915b525709804333805ea
zzl1101 <3010372332@qq.com>
2020-09-29 19:59:37 +0800

    zzl提交

--- src/router/index.js
+++ src/router/index.js
@@ -6,61 +6,66 @@ const routes = [
   {
     path: '/',
     name: 'HomeHouse',
-    component: () => import( '../views/home/HomeHouse.vue'),
-    meta:{
-      showTabbar:true,
+    component: () => import('../views/home/HomeHouse.vue'),
+    meta: {
+      showTabbar: true,
     }
   },
   {
     path: '/Coach',
     name: 'Coach',
-    component: () => import( '../views/home/Coach.vue'),
+    component: () => import('../views/home/Coach.vue'),
   },
   {
     path: '/CourseHome',
     name: 'CourseHome',
-    component: () => import( '../views/course/CourseHome.vue'),
-    meta:{
-      showTabbar:true,
+    component: () => import('../views/course/CourseHome.vue'),
+    meta: {
+      showTabbar: true,
     }
+  },
+  {
+    path: "/CourseDetails",
+    name: "CourseDetails",
+    component: () => import("../views/course/CourseDetails.vue")
   },
   {
     path: '/ExerciseHome',
     name: 'ExerciseHome',
-    component: () => import( '../views/exercise/ExerciseHome.vue'),
-    meta:{
-      showTabbar:true,
+    component: () => import('../views/exercise/ExerciseHome.vue'),
+    meta: {
+      showTabbar: true,
     }
   },
-  
+
   {
     path: '/MineHome',
     name: 'MineHome',
-    component: () => import( '../views/mine/MineHome.vue'),
-    meta:{
-      showTabbar:true,
+    component: () => import('../views/mine/MineHome.vue'),
+    meta: {
+      showTabbar: true,
     }
   },
   {
     path: '/RecordHome',
     name: 'RecordHome',
-    component: () => import( '../views/record/RecordHome.vue'),
-    meta:{
-      showTabbar:true,
+    component: () => import('../views/record/RecordHome.vue'),
+    meta: {
+      showTabbar: true,
     }
   },
   {
     path: '/class_item',
     name: 'class_item',
-    component: () => import( '../views/record/record_item/class_item.vue'),
+    component: () => import('../views/record/record_item/class_item.vue'),
   }
-  
+
 ]
 
 const router = new VueRouter({
-   mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
 })
 
 export default router
