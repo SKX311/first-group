3e7634bbe2c16ec40cc956f666cc6e57a5fb4e8a
duhao <2073307388@qq.com>
2020-09-29 16:57:29 +0800

    邮箱拖拽

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    
+    <router-view />
     <div v-show="$route.meta.showTabbar">
       <van-tabbar route>
         <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
@@ -14,20 +14,61 @@
         <van-tabbar-item icon="setting-o" to="/MineHome">我的</van-tabbar-item>
       </van-tabbar>
     </div>
-    <router-view />
-    
+    <div
+      class="box"
+      id="Mailbox"
+      @touchmove.prevent="Mailbox"
+      @click="$router.push('/Service')"
+    >
+      <van-icon name="hot-o" color="snow" size="1.5rem" />
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    Mailbox(event) {
+      var ss = document.querySelector("#Mailbox");
+      var x = event.targetTouches[0].clientX;
+      var y = event.targetTouches[0].clientY;
+      var clientW = document.documentElement.clientWidth;
+      var clientH = document.documentElement.clientHeight;
+      ss.style.left = x - 25 + "px";
+      ss.style.top = y - 25 + "px"; // 检测碰撞
+      if (x < 25) {
+        x = 25;
+      } else if (x > clientW - 25) {
+        x = clientW - 25;
+      }
+
+      if (y < 25) {
+        y = 25;
+      } else if (y > clientH - 25) {
+        y = clientH - 25;
+      }
+      ss.style.left = x - 25 + "px";
+      ss.style.top = y - 25 + "px";
+    },
+  },
+};
+</script>
+
 
 <style lang="scss">
 * {
   font-size: 0.24rem;
 }
-<<<<<<< HEAD
-// .grid{
-//   height: 0.96rem;
-// }
-=======
-
->>>>>>> fa5cef2bdd8a3c0cc06f17da49c616a6614d63ab
+.box {
+  width: 1.5rem;
+  height: 1.5rem;
+  position: absolute;
+  bottom: 3rem;
+  background-color: blue;
+  z-index: 9999;
+  border-radius: 50%;
+  text-align: center;
+  line-height: 2rem;
+  margin: 0 auto;
+}
 </style>
